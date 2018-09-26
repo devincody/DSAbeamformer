dace717783b46d5660af5b4939fd912f1e016dd1
devincody <devincody@gmail.com>
2018-09-26 15:46:32 -0700

    Updated code to do 4-bit to 8-bit unpacking with demonstration

--- sandbox/bitshift.cpp
+++ sandbox/bitshift.cpp
@@ -2,11 +2,31 @@
 #include <bitset>
 
 int main(){
-	char a = 4;
-	//a = (a << 4) >> 3;
-	//a = (a >> 4);
-	std::bitset<16>x( (int16_t) a);
-	std::cout << "a = " << x << std::endl;
+	char positive_number = 0x25;
+	char negative_number = 0xA8;
+
+
+	std::cout << "\nfirst SLOT: " << std::endl;
+	char positive_number_first_slot = (positive_number >> 4);
+	std::bitset<8>w( positive_number_first_slot);
+	std::cout << "positive_number_first_slot = " << w << std::endl;
+	
+	char negative_number_first_slot = (negative_number >> 4);
+	std::bitset<8>z(negative_number_first_slot);
+	std::cout << "negative_number_first_slot = " << z << std::endl;
+
+	std::cout << "\nsecond SLOT: " << std::endl;
+	char positive_number_second_slot = (positive_number << 4);
+	positive_number_second_slot = (positive_number_second_slot >> 4);
+	std::bitset<8>y( positive_number_second_slot);
+	std::cout << "positive_number_second_slot = " << y << std::endl;
+
+
+	char negative_number_second_slot = (negative_number << 4);
+	negative_number_second_slot = (negative_number_second_slot >> 4);
+	std::bitset<8>x(negative_number_second_slot);
+	std::cout << "negative_number_second_slot = " << x << std::endl;
+
 
 	return 0;
 }

f16c8c4fde3cb3670c1f593bf1850c502d95173d
devincody <devincody@gmail.com>
2018-09-25 19:56:20 -0700

    bit expander is working in 20 us

--- sandbox/kernelTest.cu
+++ sandbox/kernelTest.cu
@@ -8,64 +8,43 @@
 #define N_AVERAGING 16
 #define N_TIMESTEPS_PER_CALL 64
 #define N_POL 2
-#define N 1000
+#define N 1000000
 
-typedef char2 CxInt8_t;
-typedef char c4[4]; //32-bit so global memory bandwidth usage is optimal
-typedef char c8[8]; //64-bit so global memory bandwidth usage is optimal
-typedef CxInt8_t cu4[4];
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 
-
-union Sc4{
-	c4 c;
-	float s;
-};
-
-union Dc8{
-	c8 c;
-	CxInt8_t cu[4];
-	double d;
-};
-
-
-__global__
-void reduce_input(Sc4 *input, Dc8 *output, int input_size){
-	/*
-	This code takes in an array of 4-bit integers and returns an array of 8-bit integers.
-	To maximize global memory bandwidth and symplicity, two special char types are 
-	defined: c4 and c8. The size of these types are 32-bit and 64-bit respectively. These
-	enable coalesced memory accesses, but then require the gpu to handle the 4 to 8-bit
-	conversion simultaneously for 8 numbers (4 real/imaginary pairs). 
-	*/
-
-	__shared__ Sc4 shmem_in[32];
-	__shared__ Dc8 shmem_out[32];
-
-	int local_idx = threadIdx.x;
-	int global_idx = blockIdx.x * blockDim.x + threadIdx.x;
-
-	while (global_idx < input_size){
-		shmem_in[local_idx].s = input[global_idx].s; // read eight pieces of 4-bit memory into shared memory
-
-		//#pragma unroll
-		for (int i = 0; i < 4; i++){
-			char temp = shmem_in[local_idx].c[i];
-
-			// break the char into two 4-bit chunks, then convert to 8-bit
-			char high = (temp >> 4); // roll the 		most  significant 4 bits over the least significant 4 bits
-			char low = (temp << 4);  // roll the 		least significant 4 bits over the most  significant 4 bits
-			low = (low >> 4); 	 	 // roll the *new* 	most  significant 4 bits over the least significant 4 bits
-
-			// store the two 8-bit numbers to the output shared memory array
-			shmem_out[local_idx].cu[i].x = high; 
-			shmem_out[local_idx].cu[i].y = low;
-		}
-
-		output[global_idx].d = shmem_out[local_idx].d;	// write eight pieces of 8-bit memory out to global memory
-
-		global_idx += gridDim.x *blockDim.x;
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+	if (code != cudaSuccess) 
+	{
+		fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+		exit(code);
 	}
 }
+
+
+cudaEvent_t start;
+cudaEvent_t stop;
+
+#define START_TIMER() {                         \
+	gpuErrchk(cudaEventCreate(&start));       \
+	gpuErrchk(cudaEventCreate(&stop));        \
+	gpuErrchk(cudaEventRecord(start));        \
+}
+
+#define STOP_RECORD_TIMER(name) {                           \
+	gpuErrchk(cudaEventRecord(stop));                     \
+	gpuErrchk(cudaEventSynchronize(stop));                \
+	gpuErrchk(cudaEventElapsedTime(&name, start, stop));  \
+	gpuErrchk(cudaEventDestroy(start));                   \
+	gpuErrchk(cudaEventDestroy(stop));                    \
+}
+
+
+typedef char2 CxInt8_t;
+typedef char char4_t[4]; //32-bit so global memory bandwidth usage is optimal
+typedef char char8_t[8]; //64-bit so global memory bandwidth usage is optimal
+typedef CxInt8_t cuChar4_t[4];
+
 
 
 __global__
@@ -86,11 +65,11 @@ void detect_sum(cuComplex *input, cuComplex *output){
 }
 
 __global__
-void expand_input(float *input, double *output, int input_size){
+void expand_input(char *input, char *output, int input_size){
 	/*
 	This code takes in an array of 4-bit integers and returns an array of 8-bit integers.
 	To maximize global memory bandwidth and symplicity, two special char types are 
-	defined: c4 and c8. The size of these types are 32-bit and 64-bit respectively. These
+	defined: char4_t and char8_t. The size of these types are 32-bit and 64-bit respectively. These
 	enable coalesced memory accesses, but then require the gpu to handle the 4 to 8-bit
 	conversion simultaneously for 8 numbers (4 real/imaginary pairs). 
 	*/
@@ -98,17 +77,17 @@ void expand_input(float *input, double *output, int input_size){
 	__shared__ float shmem_in[32];
 	__shared__ double shmem_out[32];
 
-	c4 *char_shmem_in;
-	cu4 *char_shmem_out;
+	char4_t *char_shmem_in;
+	cuChar4_t *char_shmem_out;
 
-	char_shmem_in = reinterpret_cast<c4*>(shmem_in);
-	char_shmem_out = reinterpret_cast<cu4 *>(shmem_out);
+	char_shmem_in = reinterpret_cast<char4_t *>(shmem_in);
+	char_shmem_out = reinterpret_cast<cuChar4_t *>(shmem_out);
 
 	int local_idx = threadIdx.x;
 	int global_idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-	while (global_idx < input_size){
-		shmem_in[local_idx] = input[global_idx]; // read eight pieces of 4-bit memory into shared memory
+	while (global_idx < input_size/sizeof(float)){
+		shmem_in[local_idx] = ((float *) input)[global_idx]; // read eight pieces of 4-bit memory into shared memory
 
 		//#pragma unroll
 		for (int i = 0; i < 4; i++){
@@ -124,9 +103,9 @@ void expand_input(float *input, double *output, int input_size){
 			char_shmem_out[local_idx][i].y = low;
 		}
 
-		output[global_idx] = shmem_out[local_idx];	// write eight pieces of 8-bit memory out to global memory
+		((double *) output)[global_idx] = shmem_out[local_idx];	// write eight pieces of 8-bit memory out to global memory
 
-		global_idx += gridDim.x *blockDim.x;
+		global_idx += gridDim.x * blockDim.x;
 	}
 }
 
@@ -142,27 +121,33 @@ int main (){
 
 
 
-	// char *in = new char[N];
-	// // doubleType *out = new doubleType[N];
+	char *in = new char[N];
+	char *out = new char[2*N];
 
-	// for (int i = 0; i < N; i ++){
-	// 	in[i] = 0xE7;
-	// }
+	for (int i = 0; i < N; i ++){
+		in[i] = 0xD7;
+	}
 
-	// char *d_in;
-	// doubleType *d_out;
+	char *d_in;
+	char *d_out;
 
-	// cudaMalloc(&d_in, N*sizeof(char));
-	// cudaMalloc(&d_out, N*sizeof(doubleType));
+	gpuErrchk(cudaMalloc(&d_in, N*sizeof(char)));
+	gpuErrchk(cudaMalloc(&d_out, 2*N*sizeof(char)));
 
-	// cudaMemcpy(d_in, in, N*sizeof(char), cudaMemcpyHostToDevice);
+	gpuErrchk(cudaMemcpy(d_in, in, N*sizeof(char), cudaMemcpyHostToDevice));
 
-	// reduce_input<<<20,32>>>(d_in, d_out);
+	float gpu_time_ms_solve;
+	START_TIMER();
+	expand_input<<<1000,32>>>(d_in, d_out, N);
+	STOP_RECORD_TIMER(gpu_time_ms_solve);
 
-	// cudaMemcpy(d_out, out, N*sizeof(doubleType), cudaMemcpyDeviceToHost);
+	cudaMemcpy(out, d_out, 2*N*sizeof(char), cudaMemcpyDeviceToHost);
 
-	// for (int i = 0;)
+	for (int i = 0; i < 10; i ++){
+		std::cout << (int) out[i] << std::endl;
+	}
 
+	std::cout << "time was: " << gpu_time_ms_solve << std::endl;
 
 
 

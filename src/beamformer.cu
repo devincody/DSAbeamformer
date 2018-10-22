2232bad0c5ddee38365145ab97b84c800b20194f
devincody <devincody@gmail.com>
2018-10-22 10:07:44 -0700

    beamforming runs in streaming mode

--- src/beamformer.cu
+++ src/beamformer.cu
@@ -32,24 +32,27 @@ int main(){
 	/* CUBLAS Constants */
 	cuComplex  h_inv_max_value,  h_zero; 		//Host Values
 	cuComplex *d_inv_max_value, *d_zero; 	//Device Values
-	float  h_f_one,  h_f_zero;				//Host Values
-	float *d_f_one, *d_f_zero;				//Device Values	
 
 	h_inv_max_value.x = 1.0/MAX_VAL;
 	h_inv_max_value.y = 0;
 	h_zero.x = 0;
 	h_zero.y = 0;
-	h_f_one = 1.0;
-	h_f_zero = 0.0;
+
+	#if DEBUG
+		float  h_f_one,  h_f_zero;				//Host Values
+		float *d_f_one, *d_f_zero;				//Device Values	
+		h_f_one = 1.0;
+		h_f_zero = 0.0;
+	#endif
 
 	/***********************************
 	 *			HOST Variables		   *
 	 ***********************************/
 	CxInt8_t *A = new CxInt8_t[A_cols*A_rows*N_FREQUENCIES];
-	char *data = new char[N_BYTES_PRE_EXPANSION_PER_GEMM*N_DIRS];
-	float *beam_out = new float[N_FREQUENCIES*N_BEAMS*N_OUTPUTS_PER_GEMM];
-	gpuErrchk(cudaHostRegister(data, N_BYTES_PRE_EXPANSION_PER_GEMM*N_DIRS*sizeof(char), cudaHostRegisterPortable)); //need pinned memory
-	gpuErrchk(cudaHostRegister(beam_out, N_FREQUENCIES*N_BEAMS*N_OUTPUTS_PER_GEMM*sizeof(float), cudaHostRegisterPortable)); //need pinned memory
+	char *data = new char[(long) N_BYTES_PRE_EXPANSION_PER_GEMM*N_DIRS];
+	float *beam_out = new float[N_F_PER_DETECT*N_STREAMS];
+	gpuErrchk(cudaHostRegister(data, (long) N_BYTES_PRE_EXPANSION_PER_GEMM*N_DIRS*sizeof(char), cudaHostRegisterPortable)); //need pinned memory
+	gpuErrchk(cudaHostRegister(beam_out, N_FREQUENCIES*N_BEAMS*N_OUTPUTS_PER_GEMM*N_STREAMS*sizeof(float), cudaHostRegisterPortable)); //need pinned memory
 
 
 
@@ -58,12 +61,13 @@ int main(){
 		std::ofstream f;
 		f.open("bin/data.py");
 		f << "A = [[";
+		std::mutex file_mutex;
 
 		float *d_dedispersed;	// Data after being de-dispersed
 		float *d_vec_ones;		// Vector of all ones for de-dispersion
-		float *out_dedispersed = new float[N_BEAMS];
+		float *out_dedispersed = new float[N_BEAMS*N_STREAMS];
 		float *vec_ones = new float[N_FREQUENCIES];
-		gpuErrchk(cudaHostRegister(out_dedispersed, N_BEAMS*sizeof(float), cudaHostRegisterPortable));
+		gpuErrchk(cudaHostRegister(out_dedispersed, N_BEAMS*N_STREAMS*sizeof(float), cudaHostRegisterPortable));
 
 	#endif
 
@@ -112,40 +116,44 @@ int main(){
 	gpuErrchk(cudaMalloc(&d_B, 	N_CX_IN_PER_GEMM*N_STREAMS*sizeof(CxInt8_t)));
 	gpuErrchk(cudaMalloc(&d_C, 	N_CX_OUT_PER_GEMM*N_STREAMS*sizeof(cuComplex)));
 	gpuErrchk(cudaMalloc(&d_data, N_BYTES_PRE_EXPANSION_PER_GEMM*N_GEMMS_PER_BLOCK*N_BLOCKS_on_GPU)); 							// array for raw data
-	gpuErrchk(cudaMalloc(&d_out,  N_BEAMS*N_FREQUENCIES*N_STREAMS * sizeof(float)));			// array for detected, averaged data
+	gpuErrchk(cudaMalloc(&d_out, N_F_PER_DETECT*N_STREAMS * sizeof(float)));			// array for detected, averaged data
 
 	/* Cublas Constant Memory */
 	gpuErrchk(cudaMalloc(&d_inv_max_value, sizeof(cuComplex)));
 	gpuErrchk(cudaMalloc(&d_zero, sizeof(cuComplex)));
-	gpuErrchk(cudaMalloc(&d_f_one, sizeof(float)));
-	gpuErrchk(cudaMalloc(&d_f_zero, sizeof(float)));
+
 
 	#if DEBUG
+		gpuErrchk(cudaMalloc(&d_f_one, sizeof(float)));
+		gpuErrchk(cudaMalloc(&d_f_zero, sizeof(float)));
 		gpuErrchk(cudaMalloc(&d_dedispersed, N_BEAMS*N_STREAMS*sizeof(float)));						// array for frequency averaged data
-		gpuErrchk(cudaMalloc(&d_vec_ones, N_BEAMS*sizeof(float)));
+		gpuErrchk(cudaMalloc(&d_vec_ones, N_FREQUENCIES*sizeof(float)));
 	#endif
 
 	/* Copy constants to memory */
 	gpuErrchk(cudaMemcpy(d_A, A, A_rows*A_cols*N_FREQUENCIES*sizeof(CxInt8_t), cudaMemcpyHostToDevice));
 	gpuErrchk(cudaMemcpy(d_inv_max_value, &h_inv_max_value, sizeof(cuComplex), cudaMemcpyHostToDevice));
 	gpuErrchk(cudaMemcpy(d_zero, &h_zero, sizeof(cuComplex), cudaMemcpyHostToDevice));
-	gpuErrchk(cudaMemcpy(d_f_one, &h_f_one, sizeof(float), cudaMemcpyHostToDevice));
-	gpuErrchk(cudaMemcpy(d_f_zero, &h_f_zero, sizeof(float), cudaMemcpyHostToDevice));
+
 
 	#if DEBUG
 		gpuErrchk(cudaMemcpy(d_vec_ones, vec_ones, N_FREQUENCIES*sizeof(float), cudaMemcpyHostToDevice));
+		gpuErrchk(cudaMemcpy(d_f_one, &h_f_one, sizeof(float), cudaMemcpyHostToDevice));
+		gpuErrchk(cudaMemcpy(d_f_zero, &h_f_zero, sizeof(float), cudaMemcpyHostToDevice));
+		std::cout << "First: " << h_f_zero << " and " << h_f_one << std::endl;
+		print_data_scalar<<<1, 1>>>(d_f_one);
+		print_data_scalar<<<1, 1>>>(d_f_zero);
 	#endif
 
+	// gpuErrchk(cudaMemset(d_dedispersed, 0, N_BEAMS*N_STREAMS*sizeof(float)));
 
 	/***********************************
 	 *		Concurrency Handles		   *
 	 ***********************************/
 	cudaStream_t stream[N_STREAMS];
-	cudaStream_t HtoD_stream; 
-	gpuErrchk(cudaStreamCreate(&HtoD_stream));
 	cublasHandle_t handle[N_STREAMS];
 	std::thread thread[N_STREAMS];
-	int time[N_STREAMS];
+	int timeSlice[N_STREAMS];
 	cudaEvent_t BlockSync[N_BLOCKS_on_GPU];
 
 	for (int i = 0; i < N_STREAMS; i++){
@@ -163,9 +171,11 @@ int main(){
 	/***********************************
 	 *			TEST SIGNAL			   *
 	 ***********************************/
+
+
+	#if DEBUG
 	float test_direction;
 	char high, low;
-
 	for (int iii = 0; iii < N_DIRS; iii++){
 		test_direction = DEG2RAD(-3.5) + iii*DEG2RAD(7.0)/(N_DIRS-1);
 		for (int i = 0; i < N_FREQUENCIES; i++){
@@ -183,6 +193,10 @@ int main(){
 			}
 		}
 	}
+	#else
+	memset(data, 0x70, N_BYTES_PRE_EXPANSION_PER_GEMM*N_DIRS*sizeof(char));
+	std::cout << "BOGUS DATA " << std::endl;
+	#endif
 
 	std::cout << "done writing data" << std::endl;
 
@@ -190,6 +204,7 @@ int main(){
 	int64_t blocks_analyzed = 0;
 	int current_gemm = 0;
 	int64_t blocks_transfered = 0;
+
 
 	while (!observation_complete){
 		
@@ -270,7 +285,7 @@ int main(){
 
 				//cudaStreamSynchronize(stream[st]);
 
-				expand_input<<<1000, 32, 0, stream[st]>>>(&d_data[N_BYTES_PRE_EXPANSION_PER_GEMM*(N_GEMMS_PER_BLOCK*(blocks_transfered%N_BLOCKS_on_GPU) + timeSlice[st])],
+				expand_input<<<1000, 32, 0, stream[st]>>>(&d_data[N_BYTES_PRE_EXPANSION_PER_GEMM*(N_GEMMS_PER_BLOCK*(blocks_analyzed%N_BLOCKS_on_GPU) + timeSlice[st])],
 													      (char *) &d_B[N_CX_IN_PER_GEMM*st], 
 													      B_stride*N_FREQUENCIES);
 
@@ -285,30 +300,76 @@ int main(){
 											N_FREQUENCIES, CUDA_C_32F, CUBLAS_GEMM_DEFAULT_TENSOR_OP));
 
 				// std::cout << "hello3" << std::endl;
-				detect_sum<<<N_FREQUENCIES * N_OUTPUTS_PER_GEMM, N_BEAMS, 0, stream[st]>>>(&d_C[N_CX_OUT_PER_GEMM*st], d_out);
+				detect_sum<<<detect_dimGrid, detect_dimBlock, 0, stream[st]>>>(&d_C[N_CX_OUT_PER_GEMM*st], N_INPUTS_PER_OUTPUT, &d_out[st*N_F_PER_DETECT]);
 
-				gpuErrchk(cudaMemcpyAsync(beam_out, 
-										  d_out, N_OUTPUTS_PER_GEMM*N_FREQUENCIES*N_BEAMS*sizeof(float), 
+				gpuErrchk(cudaMemcpyAsync(&beam_out[st*N_F_PER_DETECT], 
+										  &d_out[st*N_F_PER_DETECT], N_OUTPUTS_PER_GEMM*N_FREQUENCIES*N_BEAMS*sizeof(float), 
 										  cudaMemcpyDeviceToHost,
 										  stream[st]));
 
-				// std::cout << "hello4" << std::endl;
+
 				#if DEBUG
+					gpuErrchk(cudaStreamSynchronize(stream[st]));
+					// std::cout << "hello4" << std::endl;
+					std::cout << " test data: " << beam_out[st*N_F_PER_DETECT] << std::endl;
+					std::cout << " test data: " << beam_out[st*N_F_PER_DETECT+1] << std::endl;
+					std::cout << " test data: " << beam_out[st*N_F_PER_DETECT+2] << std::endl;
+
+					#if 0
+					for (int j = 0; j < N_BEAMS; j++){
+						float ans = 0;
+						for (int i = 0; i < N_FREQUENCIES; i ++){
+							ans += beam_out[st*N_F_PER_DETECT + i*N_BEAMS + j];
+						}
+						// std::cout << "ans = " << ans << std::endl;
+						f << ans;
+						if (j != N_BEAMS - 1){
+							f << ",";
+						}
+					}
+
+					if (current_gemm != N_DIRS-1){
+						f << "],\n[";
+					} else {
+						f<< "]]"<<std::endl;
+					}
+					#else
+					// std::cout << "First:" << std::endl;
+					// print_data_scalar<<<1, 1,0, stream[st]>>>(d_f_one);
+					// print_data_scalar<<<1, 1,0, stream[st]>>>(d_f_zero);
+
+
+					print_data<<<1, 10,0, stream[st]>>>(&d_out[st*N_F_PER_DETECT]);
 					gpuBLASchk(cublasSgemv(handle[st], CUBLAS_OP_N,
 								N_BEAMS, N_FREQUENCIES,
 								d_f_one,
-								d_out, N_BEAMS,
+								&d_out[st*N_F_PER_DETECT], N_BEAMS,
 								d_vec_ones, 1,
 								d_f_zero,
-								d_dedispersed, 1));
+								&d_dedispersed[st*N_BEAMS], 1));
 
-					gpuErrchk(cudaMemcpyAsync(out_dedispersed, 
-											  d_dedispersed, N_BEAMS*sizeof(float), 
+					// gpuErrchk(cudaStreamSynchronize(stream[st]));
+					std::cout << "Second:" << std::endl;
+					print_data<<<1, 10>>>(&d_dedispersed[st*N_BEAMS]);
+					gpuErrchk(cudaMemcpyAsync(&out_dedispersed[st*N_BEAMS], 
+											  &d_dedispersed[st*N_BEAMS], N_BEAMS*sizeof(float), 
 											  cudaMemcpyDeviceToHost,
 											  stream[st]));
 
+					std::cout << "Third:" << std::endl;
+					print_data<<<1, 10, 0, stream[st]>>>(&d_dedispersed[st*N_BEAMS]);
+					gpuErrchk(cudaStreamSynchronize(stream[st]));
+
+					std::cout << "Fourth:" << std::endl;
+					print_data<<<1, 10, 0, stream[st]>>>(&d_dedispersed[st*N_BEAMS]);
+
+					std::cout << "b test data: " << out_dedispersed[st*N_BEAMS] << std::endl;
+					std::cout << "b test data: " << out_dedispersed[st*N_BEAMS+1] << std::endl;
+					std::cout << "b test data: " << out_dedispersed[st*N_BEAMS+2] << std::endl;
+
 					for (int ii = 0; ii < N_BEAMS; ii++){
-						f << out_dedispersed[ii];
+						f << out_dedispersed[st*N_BEAMS + ii];
+						// std::cout << out_dedispersed[st*N_BEAMS + ii] << ", ";
 						if (ii != N_BEAMS - 1){
 							f << ",";
 						}
@@ -317,23 +378,35 @@ int main(){
 					if (current_gemm != N_DIRS-1){
 						f << "],\n[";
 					} else {
-						f<< "]]";
+						f<< "]]"<<std::endl;
 					}
+					#endif
+
+					// gpuErrchk(cudaStreamAddCallback(stream[st], call_write_to_disk, &f, &file_mutex, current_gemm, BlockSync[st], &out_dedispersed[st*N_BEAMS], 0));
+
+					// gpuErrchk(cudaEventRecord(BlockSync[st], stream[st]));
+
+					// thread[st] = std::thread(call_write_to_disk, std::ref(f), std::ref(file_mutex), current_gemm, BlockSync[st], &out_dedispersed[st*N_BEAMS]);
+					// thread[st].detach();
 				#endif
 
-				std::cout << "timeSlice["<< st << "] = " << timeSlice[st] << " and " << current_gemm << std::endl;
-				timeSlice[st] += N_STREAMS; // increment so the next time slice is processed next
-				if (timeSlice[st] == N_GEMMS_PER_BLOCK){
+				// std::cout << "timeSlice["<< st << "] = " << timeSlice[st] << " and " << current_gemm << std::endl;
+				
+				if (timeSlice[st] == N_GEMMS_PER_BLOCK-1){
 					//This is incremented once each time slice in each block is analyzed
 					blocks_analyzed++;
 				}
+				
+				timeSlice[st] += N_STREAMS; // increment so the next time slice is processed next
+
 				if (timeSlice[st] >= N_GEMMS_PER_BLOCK){
 					timeSlice[st] -= N_GEMMS_PER_BLOCK; //wrap back to the beginning once each gemm in a block has been processed
 				}
 
 
-				if (current_gemm == N_DIRS){
+				if (current_gemm == N_DIRS-1){
 					observation_complete = 1;
+					std::cout << "obs Complete" << std::endl;
 					break;
 				}
 			}
@@ -342,61 +415,67 @@ int main(){
 		// std::cout << "cs : " << current_stream << std::endl;
 	}
 
-	#if 0
-		float max = 0;
-		float rms = 0.0;
-		int max_i = 0;
-		for (int i = 0; i < 256; i++){
-			if (C[i].x>max){
-				rms += C[i].x*C[i].x;
-				max = C[i].x;
-				max_i = i;
-			}
-			std::cout << "C[" << i <<"] = " << C[i].x << "+" << C[i].y << "j" << std::endl;
-		}
-
-		std::cout << "max(C) = " << max_i << ", " << max << std::endl;
-		std::cout << "rms(c) = " << sqrt(rms/256.0) << std::endl;
-	#endif
 
 	// cuProfilerStop();
 
 	for (int st = 0; st < N_STREAMS; st++){
-		cudaStreamSynchronize(stream[st]);
+		gpuErrchk(cudaStreamSynchronize(stream[st]));
 	}
+	std::cout << "Synchronized" << std::endl;
 
 	#if DEBUG
 		f.close();
 	#endif
 
+	std::cout << "freeing" << std::endl;
 
 	for (int i = 0; i < N_STREAMS; i++){
 		gpuErrchk(cudaStreamDestroy(stream[i]));
 		gpuBLASchk(cublasDestroy(handle[i]));
 	}
-	gpuErrchk(cudaStreamDestroy(HtoD_stream));
+
+	std::cout << "freeing cuda" << std::endl;
 
 	gpuErrchk(cudaFree(d_A));
 	gpuErrchk(cudaFree(d_C));
 	gpuErrchk(cudaFree(d_B));
 	gpuErrchk(cudaFree(d_data));
 	gpuErrchk(cudaFree(d_out));
+	gpuErrchk(cudaFree(d_inv_max_value));
+	gpuErrchk(cudaFree(d_zero));
 
 	#if DEBUG
 		gpuErrchk(cudaFree(d_dedispersed));
 		gpuErrchk(cudaFree(d_vec_ones));
+		gpuErrchk(cudaFree(d_f_one));
+		gpuErrchk(cudaFree(d_f_zero));		
 	#endif
+
+
+	std::cout << "freeing host" << std::endl;
+
+	gpuErrchk(cudaHostUnregister(data));
+	gpuErrchk(cudaHostUnregister(beam_out));
+
 
 
 	delete[] A;
 	delete[] data;
 	delete[] pos;
 	delete[] dir;
-	
+	delete[] beam_out;
+	std::cout << "freed all1" << std::endl;
+
+
 	#if DEBUG
+		gpuErrchk(cudaHostUnregister(out_dedispersed));
+		std::cout << "freed all2" << std::endl;
 		delete[] vec_ones;
+		std::cout << "freed all3" << std::endl;
 		delete[] out_dedispersed;
 	#endif
+
+	std::cout << "freed all4" << std::endl;
 
 	return 0;
 }

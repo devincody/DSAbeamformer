adb57fbee445646814eb82c59bde555027fdb312
devincody <devincody@gmail.com>
2018-10-30 22:47:54 -0700

    adding code to checkout which gpus are available

---
+++ util/devproperties.cu
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+
+	std::cout << "hi" << std::endl;
+
+	int devicesCount;
+	cudaGetDeviceCount(&devicesCount);
+	for(int deviceIndex = 0; deviceIndex < devicesCount; ++deviceIndex)
+	{
+	    cudaDeviceProp deviceProperties;
+	    cudaGetDeviceProperties(&deviceProperties, deviceIndex);
+	//     printf("Device name: %s", deviceProperties.name);
+	    std::cout <<  deviceProperties.name << std::endl;
+	}
+
+	return 0;
+
+}
